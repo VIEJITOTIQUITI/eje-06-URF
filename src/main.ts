dd2d8823c517b9790c5e5352e811c7b031472af8
VIEJITOTIQUITI <urielruelasflores@gmail.com>
2026-03-03 00:00:32 -0600

    Descripción de los cambios

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Docente: Ana, Luis, Sofía');
-  console.log('Alumno: Juan, María, Pedro');
-  console.log('Materia: Matemáticas, Historia, Biología');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
